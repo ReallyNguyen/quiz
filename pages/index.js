cee0f1a76af4503a4af8a17752a1e86d143134e4
Jordan Nguyen <jnguyen188@se14l0124-03.ad.bcit.ca>
2023-05-04 14:45:54 -0700

    figure out the friking map:

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import quizData from '/data/quizData.json'
+import styles from '@/styles/Home.module.css' 
 
 export default function Quiz() {
   const [questionIndex, setQuestionIndex] = useState(0)
   const [points, setPoints] = useState([0, 0, 0, 0])
+  const [poor, setPoor] = useState(false)
+  const [good, setGood] = useState(false)
+  const [great, setGreat] = useState(false)
 
   const handleOptionSelect = (optionIndex) => {
     const updatedPoints = [...points]
@@ -26,17 +30,111 @@ export default function Quiz() {
   const handleReset = () => {
     setQuestionIndex(0)
     setPoints([0, 0, 0, 0])
+    setPoor(false)
+    setGood(false)
+    setGreat(false)
   }
 
   const handleSubmit = () => {
-    const totalPoints = points.reduce((sum, point) => sum + point, 0)
+    const totalPoints = points.reduce((sum, point) => sum + point, 0);
+    const summary = quizData.map((question, index) => {
+      const optionIndex = question.options.findIndex(option => option.point === points[index]);
+      return `${question.question}: ${question.options[optionIndex].option}`;
+    });
+  
+    if (totalPoints === 6) {
+      setGreat(true);
+    } else if (totalPoints >= 3 && totalPoints <= 5) {
+      setGood(true);
+    } else if (totalPoints >= 0 && totalPoints <= 2) {
+      setPoor(true);
+    }
+  };
+  
+  if (great) {
+    const totalPoints = points.reduce((sum, point) => sum + point, 0);
     const summary = quizData.map((question, index) => {
       const optionIndex = question.options.findIndex(option => option.point === points[index])
-      return `${question.question}: ${question.options[optionIndex].option}`
+      return `${question.options[optionIndex].option}`
     })
-    const result = `Score: ${totalPoints} out of 6.`
-    alert(`${summary.join('\n')}\n\n${result}`)
-    handleReset()
+  
+    return (
+      <div className={styles.result_container_great}>
+        <div className={styles.great_background}>
+          <div className={styles.header}>
+            {/* <NavBar page="quiz" />  */}
+          </div>
+          <div>
+            <Image src="/results/great/great.svg" width={369} height={320} />
+          </div>
+        </div>
+        <div className={styles.group}>
+          <Image src="/results/great/three star.svg" width={143} height={36} />
+          <h1>stellarknight</h1>
+          <h3>Great Progress</h3>
+          <p className={styles.desc}>
+            Congratulations! Based on your quiz results, you have been identified and placed on team StellarKnight. It looks like you are successfully making a positive change on the planet! Keep it up and inspire others to also make an impact on the planet.
+          </p>
+        </div>
+        <div className={styles.main_results}>
+          <h1 className={styles.answer}>Your Answer</h1>
+          
+  
+          <div className={styles.quest_great}>
+            <h1 className={styles.quest}>Your Quest</h1>
+            <div className={styles.improve_great}>
+              <h2>How to improve</h2>
+
+
+
+              {summary}
+
+
+
+            </div>
+          </div>
+  
+          <div className={styles.result_buttons_great}>
+            <button onClick={handleIntro}>Redo the journey?</button>
+            <button onClick={handleHome}>Go back to home</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+
+
+  if (good) {
+    const totalPoints = points.reduce((sum, point) => sum + point, 0);
+    const summary = quizData.map((question, index) => {
+      const optionIndex = question.options.findIndex(option => option.point === points[index]);
+      return `${question.options[optionIndex].option}`;
+    });
+
+    return (
+      <div>
+        <p>{summary.join('\n')}</p>
+        <p>Score: {totalPoints} out of 60.</p>
+        <button onClick={handleReset}>Restart Quiz</button>
+      </div>
+    );
+  }
+
+  if (poor) {
+    const totalPoints = points.reduce((sum, point) => sum + point, 0);
+    const summary = quizData.map((question, index) => {
+      const optionIndex = question.options.findIndex(option => option.point === points[index]);
+      return `${question.question}: ${question.options[optionIndex].option}`;
+    });
+
+    return (
+      <div>
+        <p>{summary.join('\n')}</p>
+        <p>Score: {totalPoints} out of 6.</p>
+        <button onClick={handleReset}>Restart Quiz</button>
+      </div>
+    );
   }
 
   const { question, options, img } = quizData[questionIndex]
